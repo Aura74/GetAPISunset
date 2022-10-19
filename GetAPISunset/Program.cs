115b90ace49f57020ceda3d481d52f6ef05eb2d4
Lars Asplund <lars.asplund@hotmail.com>
2022-10-19 11:12:58 +0200

    funkarMedUpOchNer

--- GetAPISunset/Program.cs
+++ GetAPISunset/Program.cs
@@ -11,10 +11,19 @@ namespace GetAPISunset
 {
     class Program
     {
-        public static async Task<Root> GetWebApiLongLatAsync()
+        public static async Task<Root> GetWebApiLongLatAsync( int y, int m, int d)
         {
             HttpClient httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2022-10-13");
+
+            // 2022-10-13
+
+            var SunDate = new DateOnly(y,m,d);
+            //Console.WriteLine(SunDate);
+            
+            var addDays = SunDate.AddDays(1);
+            Console.WriteLine(addDays);
+
+            HttpResponseMessage response = await httpClient.GetAsync($"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date={SunDate}");
             response.EnsureSuccessStatusCode();
 
             Root wd = await response.Content.ReadFromJsonAsync<Root>();
@@ -24,20 +33,24 @@ namespace GetAPISunset
 
         static async Task Main(string[] args)
         {
-            var result = await GetWebApiLongLatAsync();
+            //for (int i = 0; i < 4; i++)
+            //{
+                var result = await GetWebApiLongLatAsync(2012, 12, 12);
 
-            Console.WriteLine($"Sunset: {result.results.sunset}");
-            Console.WriteLine($"Sunrise: {result.results.sunrise}");
+                Console.WriteLine($"Sunset: {result.results.sunset}");
+                Console.WriteLine($"Sunrise: {result.results.sunrise}");
 
-            var db = new ApplicationDbContext();
-            var colors = new Results[]
-            {
-                new Results(){sunrise = "15", sunset = "28"},
-                new Results(){sunrise = "10", sunset = "20"},
-                new Results(){sunrise = "70", sunset = "90"}
-            };
-            db.SunTime.AddRange(colors);
-            db.SaveChanges();
+                var db = new ApplicationDbContext();
+                var colors = new Results[]
+                {
+                    new Results(){sunrise = $"{result.results.sunrise}", sunset = $"{result.results.sunset}"},
+                };
+                db.SunTime.AddRange(colors);
+                db.SaveChanges();
+
+            //}
+            
+            
         }
     }
 }
