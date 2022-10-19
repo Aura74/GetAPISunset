b7677c2039c83425b023f64fc3341438f9fceb6c
Lars Asplund <lars.asplund@hotmail.com>
2022-10-19 14:06:22 +0200

    loopMedTiderODatum

--- GetAPISunset/Program.cs
+++ GetAPISunset/Program.cs
@@ -11,46 +11,102 @@ namespace GetAPISunset
 {
     class Program
     {
-        public static async Task<Root> GetWebApiLongLatAsync( int y, int m, int d)
+        public static async Task<Root> GetWebApiLongLatAsync(string SunDate)
         {
             HttpClient httpClient = new HttpClient();
 
-            // 2022-10-13
+            // 2022-10-13 // ska vara detta format
 
-            var SunDate = new DateOnly(y,m,d);
+            // convert 
+            string dt = "2010-10-04T20:12:45-5:00";
+            DateTime newDt = DateTime.Parse(dt);
+            //Console.WriteLine(newDt.ToString());
+
+            //int vadSrevDu = Convert.ToInt32(Console.ReadLine());
+            int dayCount = 3;
+            //int dayCount = vadSrevDu;
+            //Console.WriteLine($"Du skrev {dayCount}");
+
+            DateTime today = DateTime.Today;
+            string requestStartDate = today.AddDays(-dayCount).ToString("yyyy-MM-dd");
+            string requestEndDate = today.AddDays(-dayCount + 2).ToString("yyyy-MM-dd");
+
+            //Time cory
+            DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now);
+            TimeOnly timeOnly = TimeOnly.FromDateTime(DateTime.Now);
+            //Console.WriteLine($"DateOnly {dateOnly}");
+            //Console.WriteLine($"TimeOnly {timeOnly}");
+
+            //SunDate = requestStartDate;
+            //var SunDate = new DateOnly(y,m,d);
             //Console.WriteLine(SunDate);
-            
-            var addDays = SunDate.AddDays(1);
-            Console.WriteLine(addDays);
 
-            HttpResponseMessage response = await httpClient.GetAsync($"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date={SunDate}");
+            //var addDays = SunDate.AddDays(1);
+            //Console.WriteLine(addDays);
+
+            var toDayString = DateTime.Now.ToShortDateString();
+            //Console.WriteLine(toDayString);
+
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date={SunDate}";
+            //var uri2 = $"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today"; // idag
+            var uri3 = $"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&formatted=0"; // dagens datum "sunrise":"2022-10-19T05:05:35+00:00",
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
             response.EnsureSuccessStatusCode();
-
             Root wd = await response.Content.ReadFromJsonAsync<Root>();
-
             return wd;
         }
+
+        //public static string GetSunrise()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    var responce = client.GetStringAsync("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400").Result;
+        //    var Sunrise = JsonSerializer.Deserialize<SunriseSunsetDto>(responce.ToString()).Results.sunrise;
+        //    return Sunrise;
+        //}
+        //public static string GetSunset()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    var responce = client.GetStringAsync("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400").Result;
+        //    var Sunset = JsonSerializer.Deserialize<SunriseSunsetDto>(responce.ToString()).Results.sunset;
+        //    return Sunset;
+        //}
+
 
         static async Task Main(string[] args)
         {
             //for (int i = 0; i < 4; i++)
             //{
-                var result = await GetWebApiLongLatAsync(2012, 12, 12);
+            //var result = await GetWebApiLongLatAsync(2012, 8, 12);
 
-                Console.WriteLine($"Sunset: {result.results.sunset}");
-                Console.WriteLine($"Sunrise: {result.results.sunrise}");
+            DateOnly wert = DateOnly.FromDateTime(DateTime.Now);
+            for (int i = 0; i < 5; i++)
+            {
+                wert = wert.AddDays(1);
+                Console.WriteLine(wert);
 
-                var db = new ApplicationDbContext();
-                var colors = new Results[]
-                {
+
+                var result = await GetWebApiLongLatAsync(wert.ToString());
+
+            Console.WriteLine($"Sunset: {result.results.sunset}");
+            Console.WriteLine($"Sunrise: {result.results.sunrise}");
+
+
+            var db = new ApplicationDbContext();
+            var sunUpOrDownTime = new Results[]
+            {
                     new Results(){sunrise = $"{result.results.sunrise}", sunset = $"{result.results.sunset}"},
-                };
-                db.SunTime.AddRange(colors);
-                db.SaveChanges();
+            };
+            db.SunTime.AddRange(sunUpOrDownTime);
+            db.SaveChanges();
 
             //}
-            
-            
+
+
+
+                //DateOnly dateOnly = DateOnly.FromDateTime(DateTime.Now).AddDays(1);
+            }//wert
         }
     }
 }
---
+++ GetAPISunset/SunriseSunsetDto.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.Json.Serialization;
+//using System.Threading.Tasks;
+
+//namespace GetAPISunset
+//{
+//    public class SunriseSunsetDto
+//    {
+//        public Results Results { get; set; }
+//        public string Status { get; set; }
+//    }
+//    public class Results2
+//    {
+//        [JsonPropertyName("sunrise")]
+//        public string Sunrise { get; set; }
+//        [JsonPropertyName("sunset")]
+//        public string Sunset { get; set; }
+//        [JsonPropertyName("solar_noon")]
+//        public string SolarNoon { get; set; }
+//        [JsonPropertyName("day_length")]
+//        public string DayLength { get; set; }
+//        [JsonPropertyName("civil_twilight_begin")]
+//        public string CivilTwilightBegin { get; set; }
+//        [JsonPropertyName("civil_twilight_end")]
+//        public string CivilTwilightEnd { get; set; }
+//        [JsonPropertyName("nautical_twilight_begin")]
+//        public string NauticalTwilightBegin { get; set; }
+//        [JsonPropertyName("nautical_twilight_end")]
+//        public string NauticalTwilightEnd { get; set; }
+//        [JsonPropertyName("astronomical_twilight_begin")]
+//        public string AstronomicalTwilightBegin { get; set; }
+//        [JsonPropertyName("astronomical_twilight_end")]
+//        public string AstronomicalTwilightEnd { get; set; }
+//    }
+
+//}
